8db6ac700e6458ba05a64901cfff772b36c147e5
Ben <ben@Bens-MBP.lan>
2019-07-08 14:42:41 +0100

    Updates

---
+++ precache-manifest.1ab06d7d448161ffc5175f0b2c1aa616.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff884bd547208c8779e63fbbbcfd32cd",
+    "url": "/iss-tracker/index.html"
+  },
+  {
+    "revision": "c1b5284579b9063c5bdb",
+    "url": "/iss-tracker/static/css/main.aa79e619.chunk.css"
+  },
+  {
+    "revision": "f0e70e5b2936f468a720",
+    "url": "/iss-tracker/static/js/2.42a13583.chunk.js"
+  },
+  {
+    "revision": "c1b5284579b9063c5bdb",
+    "url": "/iss-tracker/static/js/main.8f01b28c.chunk.js"
+  },
+  {
+    "revision": "f310990c535894d38956",
+    "url": "/iss-tracker/static/js/runtime~main.dcf7a060.js"
+  },
+  {
+    "revision": "d5f8314fb13fc46042bb7c36debef5c6",
+    "url": "/iss-tracker/static/media/Nasa.d5f8314f.ttf"
+  },
+  {
+    "revision": "93b3c122bd67423520e2131e156d5833",
+    "url": "/iss-tracker/static/media/iss-1.93b3c122.png"
+  },
+  {
+    "revision": "da43858edb1b08ae9c97becf78cf2de7",
+    "url": "/iss-tracker/static/media/nasa-alt-logo.da43858e.png"
+  },
+  {
+    "revision": "d65b793c96911c1503b13bb9a4e7c2ca",
+    "url": "/iss-tracker/static/media/nasa-load-logo2.d65b793c.gif"
+  },
+  {
+    "revision": "6099842fdaa23fbcaa3260f73c528545",
+    "url": "/iss-tracker/static/media/nasa-logo.6099842f.png"
+  },
+  {
+    "revision": "82987827f2526914f03f1488ebda55a2",
+    "url": "/iss-tracker/static/media/nasalization-rg.82987827.ttf"
+  }
+]);
